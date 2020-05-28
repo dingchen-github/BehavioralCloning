bbe5f49807cfc605c14fc2c58243c1851887efe8
dinosaurdsc <dinosaurdsc@hotmail.com>
2020-05-28 10:36:03 +0200

    Clean up

--- model.py
+++ model.py
@@ -3,22 +3,23 @@ import csv
 import cv2
 import numpy as np
 from sklearn.utils import shuffle
-from keras.models import Sequential, Model
-from keras.layers.core import Dense, Activation, Flatten, Dropout
+from keras.models import Sequential, Model, load_model
+from keras.layers import Dense, Activation, Flatten, Dropout
 from keras.layers import Conv2D, MaxPooling2D, Lambda, Cropping2D
 import math
 import matplotlib.pyplot as plt
 
+path = './data/'
+filename = 'driving_log.csv'
+logfile = path + filename
 samples = []
-with open('./data/driving_log.csv') as csvfile:
+with open(logfile) as csvfile:
     reader = csv.reader(csvfile)
     next(reader)
     count = 0
     for row in reader:
         count += 1
         samples.append(row)
-        if count == 320:
-            break
 
 def generator(samples, batch_size=32):
     num_samples = len(samples)
@@ -26,37 +27,37 @@ def generator(samples, batch_size=32):
         shuffle(samples)
         for offset in range(0, num_samples, batch_size):
             batch_samples = samples[offset:offset+batch_size]
-
+            
             images = []
             angles = []
             for batch_sample in batch_samples:
-                name = './data/IMG/'+batch_sample[0].split('/')[-1]
-                imgBGR = cv2.imread(name)
-                center_image = cv2.cvtColor(imgBGR, cv2.COLOR_BGR2RGB)
+                name0 = path+'IMG/'+batch_sample[0].split('/')[-1]
+                imgBGR0 = cv2.imread(name0)
+                center_image = cv2.cvtColor(imgBGR0, cv2.COLOR_BGR2RGB)
+                
+                name1 = path+'IMG/'+batch_sample[1].split('/')[-1]
+                imgBGR1 = cv2.imread(name1)
+                left_image = cv2.cvtColor(imgBGR1, cv2.COLOR_BGR2RGB)
+                
+                name2 = path+'IMG/'+batch_sample[2].split('/')[-1]
+                imgBGR2 = cv2.imread(name2)
+                right_image = cv2.cvtColor(imgBGR2, cv2.COLOR_BGR2RGB)
+                
                 center_angle = float(batch_sample[3])
+                correction = 0.2 # this is a parameter to tune
+                left_angle = center_angle + correction
+                right_angle = center_angle - correction
+                
                 images.append(center_image)
+                images.append(left_image)
+                images.append(right_image)
                 angles.append(center_angle)
-
+                angles.append(left_angle)
+                angles.append(right_angle)
+            
             X_train = np.array(images)
             y_train = np.array(angles)
             yield shuffle(X_train, y_train)
-
-#             steering_center = float(row[3])
-
-#             # create adjusted steering measurements for the side camera images
-#             correction = 0.2 # this is a parameter to tune
-#             steering_left = steering_center + correction
-#             steering_right = steering_center - correction
-
-#             # read in images from center, left and right cameras
-#             path = "..." # fill in the path to your training IMG directory
-#             img_center = process_image(np.asarray(Image.open(path + row[0])))
-#             img_left = process_image(np.asarray(Image.open(path + row[1])))
-#             img_right = process_image(np.asarray(Image.open(path + row[2])))
-
-#             # add images and angles to data set
-#             car_images.extend(img_center, img_left, img_right)
-#             steering_angles.extend(steering_center, steering_left, steering_right)
 
 # augmented_images, augmented_measurements = [],[]
 # for image, measurement in zip(images, measurements):
@@ -79,13 +80,13 @@ validation_generator = generator(validation_samples, batch_size=batch_size)
 
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
-# model.add(Lambda(lambda x: x/127.5 - 1., input_shape=(ch, row, col), output_shape=(ch, row, col)))
-model.add(Cropping2D(cropping = ((70,25),(0,0)))) # crop top 70 and bottom 25 pixels, left 0 and right 0
-model.add(Conv2D(24, (5, 5), strides=(2, 2), padding='valid', activation='relu'))
-model.add(Conv2D(36, (5, 5), strides=(2, 2), padding='valid', activation='relu'))
-model.add(Conv2D(48, (5, 5), strides=(2, 2), padding='valid', activation='relu'))
-model.add(Conv2D(64, (3, 3), strides=(1, 1), padding='valid', activation='relu'))
-model.add(Conv2D(64, (3, 3), strides=(1, 1), padding='valid', activation='relu'))
+# crop top 70 and bottom 25 pixels, left 0 and right 0
+model.add(Cropping2D(cropping = ((70,25),(0,0))))
+model.add(Conv2D(24, (5,5), strides=(2,2), padding='valid', activation='relu'))
+model.add(Conv2D(36, (5,5), strides=(2,2), padding='valid', activation='relu'))
+model.add(Conv2D(48, (5,5), strides=(2,2), padding='valid', activation='relu'))
+model.add(Conv2D(64, (3,3), strides=(1,1), padding='valid', activation='relu'))
+model.add(Conv2D(64, (3,3), strides=(1,1), padding='valid', activation='relu'))
 model.add(Flatten())
 model.add(Dense(100, activation='relu'))
 model.add(Dense(50, activation='relu'))
@@ -94,8 +95,13 @@ model.add(Dense(1, activation=None))
 model.summary()
 model.compile(loss='mse', optimizer='Adam', metrics=['accuracy'])
 
-history_object = model.fit_generator(train_generator, steps_per_epoch = math.ceil(len(train_samples)/batch_size), validation_data = validation_generator,
-    nb_val_samples = math.ceil(len(validation_samples)/batch_size), nb_epoch=5, verbose=1)
+history_object = model.fit_generator(
+    train_generator,
+    steps_per_epoch = math.ceil(len(train_samples)/batch_size),
+    validation_data = validation_generator,
+    nb_val_samples = math.ceil(len(validation_samples)/batch_size),
+    nb_epoch=5,
+    verbose=1)
 model.save('./model.h5')
 
 ### print the keys contained in the history object
@@ -108,4 +114,4 @@ plt.title('model mean squared error loss')
 plt.ylabel('mean squared error loss')
 plt.xlabel('epoch')
 plt.legend(['training set', 'validation set'], loc='upper right')
-plt.show()
+plt.savefig('./loss.png')
